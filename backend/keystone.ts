fe59f84e7c6fa5bfc3fe8f61f087f03833eccbcd
Avinash Toppo <atoppo267@gmail.com>
2021-07-26 19:43:51 +0530

    SignUP flow && PasswordRequestReset

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -20,6 +20,11 @@ const {withAuth} =createAuth({
        fields:['name','email','password'],
        //TODO: Add in initial roles here
    }, 
+   passwordResetLink:{
+       async sendToken(args){
+           console.log(args);
+       }
+   }
 });
 
 //Sessions Configs
